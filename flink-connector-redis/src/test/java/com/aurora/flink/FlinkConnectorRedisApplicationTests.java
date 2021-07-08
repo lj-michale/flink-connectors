8e6f221cf9a68fffd5b0a2bdf2bc06258d03e856
ircloud <realtimecompute.luojie@gmail.com>
2021-07-08 15:32:10 +0800

    feat: flink-connector-jcache

---
+++ flink-connector-redis/src/test/java/com/aurora/flink/FlinkConnectorRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aurora.flink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlinkConnectorRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
