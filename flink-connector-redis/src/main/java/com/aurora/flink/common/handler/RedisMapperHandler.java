0e57cfe332d988c42139730a308a267bcfe0b12d
ircloud <realtimecompute.luojie@gmail.com>
2021-07-08 16:01:26 +0800

    feat: handler for create redis mapper.

---
+++ flink-connector-redis/src/main/java/com/aurora/flink/common/handler/RedisMapperHandler.java
@@ -0,0 +1,39 @@
+package com.aurora.flink.common.handler;
+
+import com.aurora.flink.common.mapper.RedisMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.lang.reflect.Constructor;
+import java.util.Map;
+
+import static com.aurora.flink.descriptor.RedisValidator.REDIS_KEY_TTL;
+
+/**
+ * handler for create redis mapper.
+ */
+public interface RedisMapperHandler extends RedisHandler {
+
+    Logger LOGGER = LoggerFactory.getLogger(RedisMapperHandler.class);
+
+    /**
+     * create a correct redis mapper use properties.
+     * @param properties to create redis mapper.
+     * @return redis mapper.
+     */
+    default RedisMapper createRedisMapper(Map<String, String> properties) {
+        String ttl = properties.get(REDIS_KEY_TTL);
+        try {
+            Class redisMapper = Class.forName(this.getClass().getCanonicalName());
+
+            if (ttl == null) {
+                return (RedisMapper) redisMapper.newInstance();
+            }
+            Constructor c = redisMapper.getConstructor(Integer.class);
+            return (RedisMapper) c.newInstance(Integer.parseInt(ttl));
+        } catch (Exception e) {
+            LOGGER.error("create redis mapper failed", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+}
